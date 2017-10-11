fd7a97e1f07d262c07959218efd9ca950f013bf2
zhufengnodejs <zhufengnodejs@126.com>
2017-10-11 17:38:18 +0800

    8.实现课程列表

--- src/containers/Home/LessonList/index.js
+++ src/containers/Home/LessonList/index.js
@@ -4,9 +4,13 @@ export default class LessonList extends Component {
   render() {
     return (
       <ul className="lesson-list">
+        <li className="category">
+          <i className="iconfont icon-book"></i>
+          <span>全部课程</span>
+        </li>
         {
           this.props.lessons.list.map((item,index)=>(
-            <li key={index}>
+            <li className="lesson-info" key={index}>
               <img src={item.url}/>
               <h3>{item.name}</h3>
               <p>{item.price}</p>
