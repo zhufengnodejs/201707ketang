29e380ae2d6df63484168c9dcb34f61c1c363a5d
zhufengnodejs <zhufengnodejs@126.com>
2017-10-11 12:42:55 +0800

    4.内容区域可以滚动

--- src/containers/Home/Carousel/index.js
+++ src/containers/Home/Carousel/index.js
@@ -1,15 +1,38 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import ReactSwipe from 'react-swipe';
-export default class Carousel extends Component{
-    render(){
-        return (
-            <div>
-              <ReactSwipe className="carousel" swipeOptions={{continuous: true}}>
-                <div>PANE 1</div>
-                <div>PANE 2</div>
-                <div>PANE 3</div>
-              </ReactSwipe>
-            </div>
-        )
-    }
+import './index.less';
+let sliders = [
+  'http://www.zhufengpeixun.cn/themes/jianmo2/images/wechat.png',
+  'http://www.zhufengpeixun.cn/themes/jianmo2/images/sjz.png',
+  'http://www.zhufengpeixun.cn/themes/jianmo2/images/react.png'
+];
+export default class Carousel extends Component {
+  render() {
+    let options = {
+      continuous: true,
+      auto:1000,
+      //每轮播一次会调用此回调方法并传入最新的索引
+      callback:(index)=>{
+
+      }
+    };
+    return (
+      <div className="sliders">
+        <ReactSwipe className="carousel" swipeOptions={}>
+          {
+            sliders.map((item, index) => (
+              <div key={index}><img src={item}/></div>
+            ))
+          }
+        </ReactSwipe>
+        <div className="dots">
+          {
+            sliders.map((item, index) => (
+              <span key={index}></span>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
 }
