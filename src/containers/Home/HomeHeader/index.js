3345bb7b6aff29cadfec04d0236ef4e3cccaa5a3
zhufengnodejs <zhufengnodejs@126.com>
2017-10-11 11:57:48 +0800

    3.编写顶部的导航

--- src/containers/Home/HomeHeader/index.js
+++ src/containers/Home/HomeHeader/index.js
@@ -2,20 +2,27 @@ import React, {Component} from 'react';
 import logo from '../../../images/logo.png';
 import './index.less'
 export default class HomeHeader extends Component {
+  constructor(){
+    super();
+    this.state = {showMenu:false};
+  }
   render() {
     return (
       <div className="header">
         <img src={logo}/>
-        <div>
-          <i className="iconfont icon-uilist"></i>
-          <i className="iconfont icon-guanbi"></i>
+        <div onClick={()=>this.setState({showMenu:!this.state.showMenu})}>
+        {
+          this.state.showMenu?<i className="iconfont icon-guanbi"></i>:<i className="iconfont icon-uilist"></i>
+        }
         </div>
-        <ul className="menu-list">
-          <li>Node课程培训</li>
-          <li>HTML5课程培训</li>
-          <li>视频课程</li>
-          <li>文档课件</li>
-        </ul>
+        {
+          this.state.showMenu? <ul className="menu-list">
+            <li>Node课程培训</li>
+            <li>HTML5课程培训</li>
+            <li>视频课程</li>
+            <li>文档课件</li>
+          </ul>:null
+        }
       </div>
     )
   }
