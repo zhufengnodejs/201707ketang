031ebb1f9cec59f2e3fb6268fd94669d04d9d94e
zhufengnodejs <zhufengnodejs@126.com>
2017-10-12 12:05:35 +0800

    10.上拉加载

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 import './index.less'
-export default class NavBar extends Component {
+class NavBar extends Component {
   render() {
     return (
       <div className="navbar">
-        <i className="iconfont icon-fanhui"></i>
+        <i onClick={()=>this.props.history.goBack()} className="iconfont icon-fanhui"></i>
         <span>{this.props.title}</span>
       </div>
     )
   }
 }
+//给NavBar的属性对象上注入属性 history location match
+export default withRouter(NavBar)
