c2b808f35915bc5aac8b677eb8d3ac0c9d8675a7
zhufengnodejs <zhufengnodejs@126.com>
2017-10-12 12:26:31 +0800

    11.个人中心

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,9 +1,12 @@
 import React,{Component} from 'react';
+import './index.less'
+import avatar from '../../images/profile.png';
 export default class Profile extends Component{
     render(){
         return (
-            <div>
-                Profile
+            <div className="profile">
+                <img src={avatar}/>
+                <span>登录</span>
             </div>
         )
     }
